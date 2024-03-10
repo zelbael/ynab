7108162cf271ae64943b3b2986717f0d6ccc4563
Reiczel Bael <reiczelbael.dev@gmail.com>
2024-03-09 22:25:19 -0800

    Initial set up for YNAB

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction
+  },
+};
+</script>
